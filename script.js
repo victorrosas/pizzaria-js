a40507c9d8f5ce2d82de566d544429192f1915ea
Victor Rosas <victor.rosas.96@hotmail.com>
2021-07-08 11:35:25 -0500

    mostrar modal

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ pizzaJson.map((item, index) => {
   
   //preencher as informações
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  // Mostrar Modal
+  pizzaItem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    c('.pizzaWindowArea').style.opacity = 0;
+    c('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
+      c('.pizzaWindowArea').style.opacity = 1;
+    }, 200);
+  });
 
   c('.pizza-area').append( pizzaItem );
 });
